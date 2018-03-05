8adb92d7778eb780961138160cdd9a25066b974d
Lê Việt Thắng <levietthang1997@gmail.com>
2018-03-05 23:47:12 +0700

    project rebuiled

--- app/src/main/java/tagroup/thangducanh/com/studiobackup/Constant.java
+++
@@ -1,9 +0,0 @@
-package tagroup.thangducanh.com.studiobackup;
-
-/**
- * Created by asus on 2/26/2018.
- */
-
-public interface Constant {
-    String IP_SERVER = "https://onfilm.000webhostapp.com";
-}
